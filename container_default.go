ef674b57874a75e19a6f0b897443603efa47b876
Zhanbolat Yerkinbay <eerzho@gmail.com>
2025-05-31 23:41:13 +0200

    feat: singleton for default

--- container_default.go
+++ container_default.go
@@ -1,23 +1,34 @@
 package simpledi
 
-// Default container
-var c = NewContainer()
+import "sync"
+
+var (
+	c    *Container
+	once sync.Once
+)
 
 // Register a dependency by key
 //   - key:     unique name for the dependency
 //   - needs:   list of dependency keys this object depends on
 //   - builder: function that returns the object instance
 func Register(key string, needs []string, builder func() any) {
-	c.Register(key, needs, builder)
+	defaultC().Register(key, needs, builder)
 }
 
 // Get a dependency by key
 //   - key: unique name of the dependency
 func Get(key string) any {
-	return c.Get(key)
+	return defaultC().Get(key)
 }
 
 // Resolve all dependencies
 func Resolve() error {
-	return c.Resolve()
+	return defaultC().Resolve()
+}
+
+func defaultC() *Container {
+	once.Do(func() {
+		c = NewContainer()
+	})
+	return c
 }
