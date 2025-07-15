f5dd65ec3b90897002fcab7d81a263edf5efa0cd
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-07-15 02:08:00 +0200

    feat: add generic methods for default container

--- container_default.go
+++ container_default.go
@@ -1,6 +1,9 @@
 package simpledi
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 var (
 	c    *Container
@@ -21,10 +24,42 @@ func Get(key string) (any, bool) {
 	return defaultC().Get(key)
 }
 
-// MustGet retrieves a dependency by key or panics if not found
+// Get a dependency by key and casts it to the specified type
+//   - key: unique name of the dependency
+func GetAs[T any](key string) (T, bool) {
+	var zero T
+
+	object, ok := Get(key)
+	if !ok {
+		return zero, false
+	}
+
+	typed, ok := object.(T)
+	if !ok {
+		return zero, false
+	}
+
+	return typed, true
+}
+
+// Get a dependency by key or panics
 //   - key: unique name of the dependency
 func MustGet(key string) any {
 	return defaultC().MustGet(key)
+}
+
+// Get a dependency by key and casts it to the specified type or panics
+//   - key: unique name of the dependency
+func MustGetAs[T any](key string) T {
+	var zero T
+
+	object := MustGet(key)
+	typed, ok := object.(T)
+	if !ok {
+		panic(fmt.Sprintf("dependency [%s] cannot be cast to %T", key, zero))
+	}
+
+	return typed
 }
 
 // Resolve all dependencies
