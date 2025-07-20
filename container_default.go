604f9efb12ede15d132555d488ecf54cfdc520ca
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-07-20 16:53:56 +0200

    feat: add new default

--- container_default.go
+++ container_default.go
@@ -1,7 +1,6 @@
 package simpledi
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -10,10 +9,6 @@ var (
 	once sync.Once
 )
 
-// Register a dependency by key.
-//   - key: unique name for the dependency
-//   - deps: list of dependency keys this object depends on
-//   - constructor: function that returns the object instance
 func Register(option Option) error {
 	return defaultC().Register(option)
 }
@@ -22,62 +17,52 @@ func MustRegister(option Option) {
 	defaultC().MustRegister(option)
 }
 
-// Get a dependency by key.
-//   - key: unique name of the dependency
-func Get(key string) (any, bool) {
+func Get(key string) (any, error) {
 	return defaultC().Get(key)
 }
 
-// Get a dependency by key and casts it to the specified type.
-//   - key: unique name of the dependency
-func GetAs[T any](key string) (T, bool) {
+func GetAs[T any](key string) (T, error) {
 	var zero T
 
-	object, ok := Get(key)
-	if !ok {
-		return zero, false
+	object, err := Get(key)
+	if err != nil {
+		return zero, err
 	}
 
 	typed, ok := object.(T)
 	if !ok {
-		return zero, false
+		return zero, errWrongType(key, zero, object)
 	}
 
-	return typed, true
+	return typed, nil
 }
 
-// Get a dependency by key or panics.
-//   - key: unique name of the dependency
 func MustGet(key string) any {
 	return defaultC().MustGet(key)
 }
 
-// Get a dependency by key and casts it to the specified type or panics.
-//   - key: unique name of the dependency
 func MustGetAs[T any](key string) T {
-	var zero T
-
-	object := MustGet(key)
-	typed, ok := object.(T)
-	if !ok {
-		panic(fmt.Sprintf("dependency [%s] cannot be cast to %T", key, zero))
+	object, err := GetAs[T](key)
+	if err != nil {
+		panic(err)
 	}
-
-	return typed
+	return object
 }
 
-// Resolve all dependencies.
 func Resolve() error {
 	return defaultC().Resolve()
 }
 
-// Resolve all dependencies or panic.
 func MustResolve() {
 	defaultC().MustResolve()
 }
 
-func Reset() {
-	defaultC().Reset()
+func Reset() error {
+	return defaultC().Reset()
+}
+
+func MustReset() {
+	defaultC().MustReset()
 }
 
 func defaultC() *Container {
