2a9314fbe523340b42d5e0d0673af2d2a85123eb
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-11-08 00:58:35 +0100

    test: add assert funcs

---
+++ helper_test.go
@@ -0,0 +1,62 @@
+package simpledi_test
+
+import (
+	"errors"
+	"testing"
+)
+
+func assertSameInstance[T comparable](t *testing.T, got, want *T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got: %p, want: %p", got, want)
+	}
+}
+
+func assertError(t *testing.T, fn func() error, wants ...error) {
+	t.Helper()
+	err := fn()
+	for _, want := range wants {
+		if !errors.Is(err, want) {
+			t.Errorf("got: %v, want: %v", err, want)
+		}
+	}
+}
+
+func assertNoError(t *testing.T, fn func() error) {
+	t.Helper()
+	if err := fn(); err != nil {
+		t.Errorf("got: %v, want: no error", err)
+	}
+}
+
+func assertPanic(t *testing.T, fn func(), want error) {
+	t.Helper()
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf("got: no panic, want: panic(%v)", want)
+			return
+		}
+
+		err, ok := r.(error)
+		if !ok {
+			t.Errorf("got: %T, want: error", r)
+			return
+		}
+
+		if !errors.Is(err, want) {
+			t.Errorf("got: %v, want: %v", err, want)
+		}
+	}()
+	fn()
+}
+
+func assertNoPanic(t *testing.T, fn func()) {
+	t.Helper()
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("got: panic(%v), want: no panic", r)
+		}
+	}()
+	fn()
+}
