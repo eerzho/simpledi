d2a4acc3f9342d2b87ff7ce3b76dc06340c30aff
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-11-08 01:36:58 +0100

    test: add tests for close

--- close_test.go
+++ close_test.go
@@ -27,10 +27,6 @@ func Test_Close_Empty_Container(t *testing.T) {
 	assertNoError(t, simpledi.Close)
 }
 
-func Test_Close_Without_Resolve(t *testing.T) {
-
-}
-
 func Test_Close_Multiple_Errors(t *testing.T) {
 	someError1 := errors.New("some error 1")
 	simpledi.Set(simpledi.Definition{
@@ -83,6 +79,55 @@ func Test_Close_Mixed_Success_And_Error(t *testing.T) {
 }
 
 func Test_Close_Reverse_Order(t *testing.T) {
+	var callOrder []string
+
+	simpledi.Set(simpledi.Definition{
+		ID: "first",
+		New: func() any {
+			return "first"
+		},
+		Close: func() error {
+			callOrder = append(callOrder, "first")
+			return nil
+		},
+	})
+	simpledi.Set(simpledi.Definition{
+		ID: "second",
+		New: func() any {
+			return "second"
+		},
+		Close: func() error {
+			callOrder = append(callOrder, "second")
+			return nil
+		},
+	})
+	simpledi.Set(simpledi.Definition{
+		ID: "third",
+		New: func() any {
+			return "third"
+		},
+		Close: func() error {
+			callOrder = append(callOrder, "third")
+			return nil
+		},
+	})
+	simpledi.Resolve()
+
+	simpledi.Close()
+
+	if len(callOrder) != 3 {
+		t.Errorf("got: %d calls, want: 3 calls", len(callOrder))
+		return
+	}
+	if callOrder[0] != "third" {
+		t.Errorf("got: %v, want: first call to be 'third'", callOrder[0])
+	}
+	if callOrder[1] != "second" {
+		t.Errorf("got: %v, want: second call to be 'second'", callOrder[1])
+	}
+	if callOrder[2] != "first" {
+		t.Errorf("got: %v, want: third call to be 'first'", callOrder[2])
+	}
 }
 
 func Test_Close_Nil_Close_Function(t *testing.T) {
@@ -96,9 +141,6 @@ func Test_Close_Nil_Close_Function(t *testing.T) {
 	simpledi.Resolve()
 
 	assertNoError(t, simpledi.Close)
-}
-
-func Test_Close_State_Reset(t *testing.T) {
 }
 
 func Test_Close_Multiple_Times(t *testing.T) {
