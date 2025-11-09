b5958b85f1c7dc9981a883a8d7c2247bd22f2aa4
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-11-09 12:43:48 +0100

    test: add new tests for set

---
+++ set_test.go
@@ -0,0 +1,46 @@
+package simpledi_test
+
+import (
+	"testing"
+
+	"github.com/eerzho/simpledi"
+)
+
+func Test_Err_Container_Resolved(t *testing.T) {
+	defer simpledi.Close()
+
+	assertNoPanic(t, func() {
+		simpledi.Resolve()
+	})
+
+	assertPanic(t, func() {
+		simpledi.Set(simpledi.Definition{
+			ID: "service_1",
+			New: func() any {
+				return &testServiceImpl1{}
+			},
+		})
+	}, simpledi.ErrContainerResolved)
+}
+
+func Test_Err_ID_Required(t *testing.T) {
+	defer simpledi.Close()
+
+	assertPanic(t, func() {
+		simpledi.Set(simpledi.Definition{
+			New: func() any {
+				return &testServiceImpl1{}
+			},
+		})
+	}, simpledi.ErrIDRequired)
+}
+
+func Test_Err_New_Required(t *testing.T) {
+	defer simpledi.Close()
+
+	assertPanic(t, func() {
+		simpledi.Set(simpledi.Definition{
+			ID: "service_2",
+		})
+	}, simpledi.ErrNewRequired)
+}
